9f535ab157e9cc00f96b2708428c1216d683ae3d
ring19990528 <ring3884@gmail.com>
2017-10-03 16:18:53 +0800

    first commit

---
+++ src/com/ring/Hello.java
@@ -0,0 +1,14 @@
+package com.ring;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello World");
+    Person p = new Person();
+    p.sayHello();
+    
+    
+	}
+
+}
