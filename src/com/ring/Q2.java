4f547606aea567d80a1db9e653a1eaecbbe839ad
ring19990528 <ring3884@gmail.com>
2017-11-12 16:58:07 +0800

    Q2 correct!

--- src/com/ring/Q2.java
+++ src/com/ring/Q2.java
@@ -10,11 +10,14 @@ public class Q2 {
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
 		int n = Integer.parseInt(line);
-		for (int i = 1;i <= n;i = i+1){
+		for (int i = 1;i <= n;i = i+1){ 
+			for (int j = 1;j<= n;j=j+1) {
+			System.out.print("*");
 			
-			System.out.println((n * ));
 		}
+			System.out.println();
+			
+		}	
 		
 	}
-
 }
