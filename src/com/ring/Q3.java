62621305a7cc56998157ba01c44636536750401d
ring19990528 <ring3884@gmail.com>
2017-11-13 22:35:17 +0800

    Q3 finish

--- src/com/ring/Q3.java
+++ src/com/ring/Q3.java
@@ -2,28 +2,25 @@ package com.ring;
 
 import java.util.Scanner;
 
-public class Q2 {
+public class Q3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//輸入數字
-		//ex.輸入三
-		//出現
-		//***
-		//***
-		//***
 		System.out.println("請輸入一個數字:");
 		Scanner scanner = new Scanner(System.in);
 		String line = scanner.nextLine();
 		int n = Integer.parseInt(line);
-		for (int i = 1;i <= n;i = i+1){ 
-			for (int j = 1;j<= n;j=j+1) {
+		for (int i = 1; i <= n; i = i + 1) {
+			for (int j = 1; j <= n; j = j + 1) {
+			if (j <= i) {
 			System.out.print("*");
+				}
+			if (i < j) {
+				System.out.print("#");
+			}
 			
-		}
+			}
 			System.out.println();
-			
-		}	
-		
+		}
 	}
 }
