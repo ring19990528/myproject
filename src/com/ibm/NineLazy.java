76109ea0895ef93317a3c994f351707a863d5859
ring19990528 <ring3884@gmail.com>
2017-12-19 15:19:49 +0800

    midterm practice

--- src/com/ibm/NineLazy.java
+++ src/com/ibm/NineLazy.java
@@ -5,14 +5,18 @@ public class NineLazy {
 	public static void main(String[] args) {
 		for (int j = 1; j < 10; j = j + 1) {
 			for (int i = 2; i < 10; i = i + 1) {
-				if ((i * j) < 10) {
-					System.out.print(i + "*" + j + "=" + " " + (i * j)+" ");
-			}else{
-				System.out.print(i + "*" + j + "=" + (i * j)+" ");
+				if (j > 3 & j < 9) {
+					System.out.print("..." + "\t");
+				} else {
+					if ((i * j) < 10) {
+						System.out.print(i + "*" + j + "=" + " " + (i * j) + "\t");
+					} else {
+						System.out.print(i + "*" + j + "=" + (i * j) + "\t");
+					}
+				}
 			}
-		}System.out.println();
+			System.out.println();
+		}
 
-	}
-		
 	}
 }
