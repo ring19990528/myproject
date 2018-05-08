a760ee441063d2abe48a23504f2972dfb3a194a2
ring19990528 <ring3884@gmail.com>
2018-05-08 16:28:36 +0800

    List,Set

---
+++ src/com/java2/collection/Tester1.java
@@ -0,0 +1,75 @@
+package com.java2.collection;
+
+import java.io.ObjectStreamException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.concurrent.DelayQueue;
+
+import org.w3c.dom.NameList;
+
+public class Tester1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		List<GreekLetter> letterList = new ArrayList<>();
+//	    letterList.add(new GreekLetter("Gamma",  3));
+//	    letterList.add(new GreekLetter("Omega", 24));
+//	    letterList.add(new GreekLetter("Alpha",  1));
+//
+//
+//	    Object[] objArray = letterList.toArray();
+//
+//
+//	    for (int i = 0; i < objArray.length; i++) {
+//	      System.out.println(objArray[i]);
+//	    }
+//	  }
+//
+//	  static class GreekLetter {
+//
+//	    private String letter;
+//	    private Integer position;
+//
+//	    public GreekLetter(String letter, Integer position) {
+//	      this.letter = letter;
+//	      this.position = position;
+//	    }
+//
+//	    @Override
+//	    public String toString() {
+//	      return "{" +
+//	              "letter='" + letter + '\'' +
+//	              ", position=" + position +
+//	              '}';
+//	    }
+		
+//		
+//		List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+//		list.remove(0);
+//		list.remove(1);
+//		list.remove(2);
+//		list.remove(3);
+//		Object[] objArray = list.toArray();
+//		for (int i = 2; i < objArray.length; i--) {
+//		    System.out.print(objArray[i]);
+		
+		
+//		 Set<String> nameSet = new TreeSet<>(Arrays.asList("Mr.Green", "Mr.Yellow", "Mr.Red"));
+//		 for(String name: nameSet){
+//			 System.out.print(name);			 
+//		 }
+		Set<Integer> set = new HashSet<>(Arrays.asList("6" , "postgres" , "sqlite" , "oracle" ,"mongodb","postgres","mssql"));
+		
+		
+		
+		}
+	}
+
+
