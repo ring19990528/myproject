cf2f174ac7ef66637a59a0122a972a53200377dd
ring19990528 <ring3884@gmail.com>
2018-05-22 16:13:33 +0800

    Snow

---
+++ src/com/java2/snowing/SnowTester.java
@@ -0,0 +1,19 @@
+package com.java2.snowing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SnowTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Snow> list = new ArrayList<>();
+		for (int i = 0; i < 100; i++) {
+		Snow snow = new Snow();
+		snow.start();
+		list.add(snow);
+		}
+		
+	}
+
+}
