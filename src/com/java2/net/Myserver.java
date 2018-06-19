ce3c1b2df3751c54f106e811554f9a7ad06e9110
ring19990528 <ring3884@gmail.com>
2018-06-19 14:42:10 +0800

    Chat

--- src/com/java2/net/Myserver.java
+++ src/com/java2/net/Myserver.java
@@ -1,10 +1,36 @@
 package com.java2.net;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Myserver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ServerSocket server;
+		try {
+			server = new ServerSocket(9958);
+			Socket socket = server.accept();
+			System.out.println("connected");
+			InputStream is = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader in = new BufferedReader(isr);
+			String line = in.readLine();
+			System.out.println(line);
+			String tokens[] = line.split(":");
+			String command = tokens[0];
+			String option = tokens[1];
+			System.out.println(command+"/"+option);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		}
+		
 	}
 
 }
