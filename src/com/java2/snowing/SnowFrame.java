e1ecf9ef53d9624ca4de21d1be1c105c68d8d250
ring19990528 <ring3884@gmail.com>
2018-06-19 14:35:02 +0800

    ChatServer

--- src/com/java2/snowing/SnowFrame.java
+++ src/com/java2/snowing/SnowFrame.java
@@ -1,5 +1,41 @@
 package com.java2.snowing;
 
-public class SnowFrame {
+import java.awt.Canvas;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JFrame;
+
+public class SnowFrame extends JFrame {
+	public SnowFrame() {
+		setSize(850, 650);
+		setLocation(500, 200);
+		add(Canvas);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+
+	}
+
+	public class SnowCanvas {
+		List<Snow> snows = new ArrayList<>();
+
+		public SnowCanvas() {
+			for (int i = 0; i < 100; i++) {
+				Snow snow = new Snow(SnowCanvas.this);
+				snows.add(snow);
+
+			}
+		}
+
+		@Override
+		public void paint(Graphics g) {
+			for (int i = 0; i < 100; i++) {
+				Snow snow = snows.get(i);
+				g.drawString(雪,snow x,snow y);
+
+			}
+
+		}
+
+	}
 }
